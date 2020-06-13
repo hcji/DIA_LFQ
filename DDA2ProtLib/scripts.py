2ad18a4e85267a334de0f7326f9ed23ece62435c
hcji <ji.hongchao@foxmail.com>
2020-06-13 16:32:49 +0800

    initial

---
+++ DDA2ProtLib/scripts.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jun 13 10:28:55 2020
+
+@author: hcji
+"""
+
+import subprocess
+
+
+DDA_File = 'F:/PXD002952/HYE124_TTOF6600_DDA_1ug_Ecoli_lgillet_I150212_001.wiff'
+OUT_Dir = 'F:/LFQBench_Data'
+
+# convert and to centroid data
+OUT_File = OUT_Dir + '/' + DDA_File.split('/')[-1].replace('wiff', 'MzXML')
+cmdline = 'qtofpeakpicker' 
+cmdline += ' --resolution=20000' 
+cmdline += ' --area=1 --threshold=1 --smoothwidth=1.1'
+cmdline += ' --in ' + DDA_File 
+cmdline += ' --out ' + OUT_File
+subprocess.call(cmdline)
+
+# Reduce fragment ion spectrum complexity by keeping only the top 150 peaks
+OUT_File1 = OUT_Dir + '/_' + DDA_File.split('/')[-1].replace('wiff', 'MzXML')
+cmdline = 'msconvert '
+cmdline += ' ' + OUT_File + ' --mzXML'
+cmdline += ' -o ' + OUT_Dir
+cmdline += ' --filter "threshold count 150 most-intense"'
+cmdline += ' --outfile ' + OUT_File1
+subprocess.call(cmdline)
+
---
+++ ReadMe.md
@@ -0,0 +1,23 @@
+## DIA_LFQ
+
+This repository aims to collect scripts for a standard pipeline for label-free quantification with data-independent acquisition mass spectrometry. This repository is still in development.
+
+### 1. Building assay libraries with DDA
+
+This step follows the protocols given by the [reference](https://www.nature.com/articles/nprot.2015.015). The scripts are included in the *DDA2ProtLib* folder
+
+#### Depends:
+
+(1) [**ProteoWizard**](http://proteowizard.sourceforge.net/)
+
+(2) [**X!Tandem**](https://www.thegpm.org/tandem/)
+
+(3) [**Comet**](http://comet-ms.sourceforge.net/)
+
+
+
+### 2. Data process with OpenSWATH workflow
+
+
+
+### 3. Post-process for label-free quantification
